5bd8abc974dd376c6847a440b0b76e00812707b4
Jigar Makawana <jigarmakwana34065@gmail.com>
2024-02-04 00:15:12 +0530

    modified file

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -53,19 +53,24 @@ const Contact = () => {
                             <form onSubmit={(e) => handleClick(e)}>
                                 <ul className='flex'>
                                     <li>
+                                        <i class="fa-solid fa-user"></i>
                                         <input type="text" placeholder='Name' onChange={(e) => setname(e.target.value)} value={name} />
                                     </li>
                                     <li>
+                                        <i class="fa-regular fa-building"></i>
                                         <input type="text" placeholder='Company' onChange={(e) => setCompany(e.target.value)} value={company} />
                                     </li>
                                     <li>
+                                        <i class="fa-solid fa-envelope"></i>
                                         <input type="text" placeholder='Email' onChange={(e) => setEmail(e.target.value)} value={email} />
                                     </li>
                                     <li>
+                                        <i class="fa-solid fa-phone-volume"></i>
                                         <input type="text" placeholder='Phone Number' onChange={(e) => setPhone(e.target.value)} value={phone} />
                                     </li>
                                 </ul>
                                 <div className="massage">
+                                    <i class="fa-solid fa-pen"></i>
                                     <textarea name="" id="" cols="30" rows="10" onChange={(e) => setMassage(e.target.value)} value={massage}></textarea>
                                 </div>
                                 <div className="btn">
